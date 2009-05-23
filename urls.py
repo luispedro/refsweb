2ada05fede37b3d99863e6d0032a21a7cb073472
Luís Pedro Coelho <lpc@cmu.edu>
2009-05-23 11:24:16 -0400

    Add admin interface

--- urls.py
+++ urls.py
@@ -1,9 +1,5 @@
 from django.conf.urls.defaults import *
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
     # Example:
     # (r'^refsweb/', include('refsweb.foo.urls')),
@@ -13,5 +9,5 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/(.*)', admin.site.root),
+    (r'^admin/(.*)', admin.site.root),
 )
