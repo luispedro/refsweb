4255cd1c54206f76f40acb3b9aec85509c604967
Luís Pedro Coelho <lpc@cmu.edu>
2009-05-23 11:23:57 -0400

    Implement a simple reference lookup

--- wikirefs/urls.py
+++ wikirefs/urls.py
@@ -5,9 +5,14 @@
 from __future__ import division, with_statement
 
 from django.conf.urls.defaults import *
+from django.contrib import admin
+import models
+admin.autodiscover()
+admin.site.register(models.BibTexEntry)
 
 urlpatterns = patterns('wikirefs',
     (r'^$', 'index.index'),
-    (r'^doi/$', 'views.get_doi'),
+    (r'^doi/(.*)/(.*)$', 'views.get_doi'),
+    (r'^admin/(.*)', admin.site.root),
 )
 
--- wikirefs/views.py
+++ wikirefs/views.py
@@ -1,36 +1,39 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2009 Luís Pedro Coelho <lpc@cmu.edu>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published
-# by the Free Software Foundation; either version 2 of the License,
-# or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-# 02110-1301, USA.
+# Part of refsweb
+# License: GPL 2 or later (see LICENSE file)
 
 from __future__ import division, with_statement
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from .models import BibTexEntry
 
-def get_doi(request):
+def get_doi(request, doi, format):
     '''
     get_doi
 
     Lookup a reference based on its DOI.
     '''
-    r = HttpResponse()
-    if request.GET and 'doi' in request.GET:
-        doi = request.GET['doi'] 
-        r.write('<h1>Query</h1><p>You looked for doi:%s.</p>' % request.GET['doi'])
-    else:
-        r.write('<h1>Hello, world.</h1><p>No GET')
-    return r
+    if format != 'bibtex':
+        r = HttpResponse()
+        r.write('Sorry mate, I only know how to display in BibTeX')
+        return r
+    results = BibTexEntry.objects.filter(doi=doi)
+    if not results:
+        r = HttpResponse()
+        r.write('<p>Sorry mate, not found.')
+        return r
+    if len(results) > 1:
+        r = HttpResponse()
+        r.write('<p>Sorry mate, this is all very confusing.</p>')
+        return r
+    b = results[0]
+    bibvalues = b.__dict__.items()
+    return render_to_response('wikirefs/templates/bibtex.html',
+        {
+            'bibtype' : 'article',
+            'bibcode' : 'wikirefs_download',
+            'bibentries' : bibvalues,
+        })
 
 
