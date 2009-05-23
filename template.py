b577ec8339094582078ec071f38c9c819f72e11e
Luís Pedro Coelho <lpc@cmu.edu>
2009-05-22 23:42:57 -0400

    Template for the project

---
+++ template.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2009 Luís Pedro Coelho <lpc@cmu.edu>
+# Part of refsweb
+# License: GPL 2 or later (see LICENSE file)
+from __future__ import division, with_statement
+
