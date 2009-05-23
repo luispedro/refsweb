b3d1ddf359c51b55c8bd5ee768c88c5aacd351dd
Luís Pedro Coelho <lpc@cmu.edu>
2009-05-23 00:25:09 -0400

    Simple view

--- wikirefs/urls.py
+++ wikirefs/urls.py
@@ -1,6 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2009 Luís Pedro Coelho <lpc@cmu.edu>
+# Part of refsweb
+# License: GPL 2 or later (see LICENSE file)
+from __future__ import division, with_statement
+
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('wikirefs.index',
-    (r'^$', 'index'),
+urlpatterns = patterns('wikirefs',
+    (r'^$', 'index.index'),
+    (r'^doi/$', 'views.get_doi'),
 )
 
