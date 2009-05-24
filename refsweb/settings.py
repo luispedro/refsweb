fd00093ff53abd4452041a58167792a8076fd67b
Luís Pedro Coelho <lpc@cmu.edu>
2009-05-24 15:50:18 -0400

    Reorganise: no code in top-level

--- refsweb/settings.py
+++ refsweb/settings.py
@@ -10,7 +10,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '/home/luispedro/projects/refsweb/django.db'             # Or path to database file if using sqlite3.
+DATABASE_NAME = '/home/luispedro/projects/refsweb/refsweb/django.db'             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
@@ -69,10 +69,10 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/luispedro/projects/refsweb/',
+    '/home/luispedro/projects/refsweb/refsweb/',
 )
 
-DJAPIAN_DATABASE_PATH = '/home/luispedro/projects/refsweb/djapian_data/'
+DJAPIAN_DATABASE_PATH = '/home/luispedro/projects/refsweb/refsweb/djapian_data/'
 
 INSTALLED_APPS = (
     'django.contrib.auth',
